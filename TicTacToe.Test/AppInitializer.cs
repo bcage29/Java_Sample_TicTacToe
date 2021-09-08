86cce5a982c1407a1714a23035cc714d68f3ba93
Ali Reza Farahnak <alfarahn@microsoft.com>
2021-09-08 19:26:12 +0200

    - Test implemented using Xamarin.UITest using the Android Studio Build

---
+++ TicTacToe.Test/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TicTacToe.Test
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android
+                    .Debug()
+                    .KeyStore("C:/Users/alfarahn/.android/debug.keystore", "android", "android", "androiddebugkey")
+                    .InstalledApp("com.example.sampleapp")
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ TicTacToe.Test/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TicTacToe.Test
+{
+    [TestFixture(Platform.Android)]
+  //  [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void TicTacToeTest()
+        {
+            app.Repl();
+            app.Tap(b => b.Marked("btn_0"));
+            app.Tap(b => b.Marked("btn_1"));
+            app.Tap(b => b.Marked("btn_2"));
+            app.Tap(b => b.Marked("btn_3"));
+            app.Tap(b => b.Marked("btn_4"));
+            app.Tap(b => b.Marked("btn_5"));
+;
+            app.WaitForElement(c => c.Marked("resetGame"));
+
+            Assert.IsTrue(true);
+        }
+    }
+}
